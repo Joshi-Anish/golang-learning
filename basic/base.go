7e9a28deb70810a0b489b0c46dc91a7a7910ad7e
Joshi-Anish <anish.221450@ncit.edu.np>
2026-04-27 00:33:47 +0545

    updated

--- basic/base.go
+++ basic/base.go
@@ -3,5 +3,7 @@ package main
 import "fmt"
 
 func main() {
+	ab, ac := 22, 12
 	fmt.Println("Hello Go")
+	fmt.Println(ab, ac)
 }
--- basic/investment calculator/investment_calculator.go
+++ basic/investment calculator/investment_calculator.go
@@ -11,8 +11,13 @@ func main() {
 	var expectedReturnRate = 5.5
 	var years = 10
 
+	const inflation = 6.5
+
 	var futureValue = float64(intialInvestment) * math.Pow(1+expectedReturnRate/100, float64(years))
 
 	fmt.Println(futureValue)
 
+	var inflationValue = futureValue / math.Pow(1+inflation/100, float64(years))
+
+	fmt.Println(inflationValue)
 }
