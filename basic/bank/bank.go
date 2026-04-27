920199f3e0e0e3bb03791af28224da1ed49c790d
Joshi-Anish <anish.221450@ncit.edu.np>
2026-04-28 00:36:31 +0545

    for loop used and break and continue in loop used

--- basic/bank/bank.go
+++ basic/bank/bank.go
@@ -9,38 +9,57 @@ func main() {
 	Balance := 1000.0
 
 	fmt.Println("------Welcome to GO bank-----------")
-	fmt.Println("What do you want to do?")
-	fmt.Println("1.Check Balance")
-	fmt.Println("2.Deposit money")
-	fmt.Println("3.Withdeaw money")
-	fmt.Println("4.Exit")
 
-	var choice int
-	fmt.Scan(&choice)
-	fmt.Println("your choice is:", choice)
+	//infinite loop break lae loop end garxa
+	for {
+		fmt.Println("What do you want to do?")
+		fmt.Println("1.Check Balance")
+		fmt.Println("2.Deposit money")
+		fmt.Println("3.Withdeaw money")
+		fmt.Println("4.Exit")
 
-	if choice == 1 {
-		fmt.Println("Your balance is :", Balance)
-	} else if choice == 2 {
+		var choice int
+		fmt.Scan(&choice)
+		fmt.Println("your choice is:", choice)
 
-		var deposit float64
-		fmt.Print("How much you want to deposit")
-		fmt.Scan(&deposit)
+		if choice == 1 {
+			fmt.Println("Your balance is :", Balance)
+		} else if choice == 2 {
 
-		fmt.Println("you deposited:", deposit)
-		Balance += deposit
-		fmt.Println("you New balance is  :", Balance)
+			var deposit float64
+			fmt.Print("How much you want to deposit")
+			fmt.Scan(&deposit)
 
-	} else if choice == 3 {
-		var withdraw float64
-		fmt.Println("How much you want to withdraw :")
-		fmt.Scan(&withdraw)
+			if deposit <= 0 {
+				fmt.Print("Must deposit more than 0")
+				continue
+			}
+			fmt.Println("you deposited:", deposit)
+			Balance += deposit
+			fmt.Println("you New balance is  :", Balance)
 
-		if withdraw > Balance {
-			fmt.Println("Insufficent amount")
-		} else {
+		} else if choice == 3 {
+			var withdraw float64
+			fmt.Println("How much you want to withdraw :")
+			fmt.Scan(&withdraw)
+
+			if withdraw <= 0 {
+				fmt.Print("Enter amount more than 0")
+				continue
+			}
+			if withdraw > Balance {
+				fmt.Println("Insufficent amount")
+				continue
+			}
+
 			Balance -= withdraw
 			fmt.Println("your new Balance is :", Balance)
+
+		} else {
+			println("goodbye")
+			break
 		}
+
 	}
+	fmt.Print("Thnaks for choising our bank")
 }
