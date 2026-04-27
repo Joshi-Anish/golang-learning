28900526cbd1331029c45d38ed66119cbd3baee2
Joshi-Anish <anish.221450@ncit.edu.np>
2026-04-28 00:14:08 +0545

    if else statemenet practice using banking task

---
+++ basic/bank/bank.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//variabble declaration
+	Balance := 1000.0
+
+	fmt.Println("------Welcome to GO bank-----------")
+	fmt.Println("What do you want to do?")
+	fmt.Println("1.Check Balance")
+	fmt.Println("2.Deposit money")
+	fmt.Println("3.Withdeaw money")
+	fmt.Println("4.Exit")
+
+	var choice int
+	fmt.Scan(&choice)
+	fmt.Println("your choice is:", choice)
+
+	if choice == 1 {
+		fmt.Println("Your balance is :", Balance)
+	} else if choice == 2 {
+
+		var deposit float64
+		fmt.Print("How much you want to deposit")
+		fmt.Scan(&deposit)
+
+		fmt.Println("you deposited:", deposit)
+		Balance += deposit
+		fmt.Println("you New balance is  :", Balance)
+
+	} else if choice == 3 {
+		var withdraw float64
+		fmt.Println("How much you want to withdraw :")
+		fmt.Scan(&withdraw)
+
+		if withdraw > Balance {
+			fmt.Println("Insufficent amount")
+		} else {
+			Balance -= withdraw
+			fmt.Println("your new Balance is :", Balance)
+		}
+	}
+}
---
+++ basic/bank/go.mod
@@ -0,0 +1,3 @@
+module bank.com/bank-calculator
+
+go 1.25.0
