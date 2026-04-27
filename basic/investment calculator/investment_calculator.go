4e0c8ca904f8bee3cd4358a0989af55069677ce6
Joshi-Anish <anish.221450@ncit.edu.np>
2026-04-27 11:36:07 +0545

    User input in investment_calculator and completion of investment calculator

--- basic/investment calculator/investment_calculator.go
+++ basic/investment calculator/investment_calculator.go
@@ -7,17 +7,26 @@ import (
 
 func main() {
 
-	var intialInvestment = 1000
-	var expectedReturnRate = 5.5
-	var years = 10
+	var intialInvestment float64
+	var expectedReturnRate float64
+	var years int
 
 	const inflation = 6.5
 
-	var futureValue = float64(intialInvestment) * math.Pow(1+expectedReturnRate/100, float64(years))
+	fmt.Print("Welcome to Investment Calclator\n")
+	//user input
+	fmt.Print("enter intial Investment:")
+	fmt.Scan(&intialInvestment)
 
+	fmt.Print("enter expected return rate:")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("enter total no years:")
+	fmt.Scan(&years)
+
+	var futureValue = float64(intialInvestment) * math.Pow(1+expectedReturnRate/100, float64(years))
 	fmt.Println(futureValue)
 
 	var inflationValue = futureValue / math.Pow(1+inflation/100, float64(years))
-
 	fmt.Println(inflationValue)
 }
