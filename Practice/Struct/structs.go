93d63932f9f52b0db609212c4201ba9adf67f42d
Joshi-Anish <anish.221450@ncit.edu.np>
2026-04-29 14:14:57 +0545

    struct used

--- Practice/Struct/structs.go
+++ Practice/Struct/structs.go
@@ -2,16 +2,32 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+type user struct {
+	firstName string
+	lastName  string
+	birthDate string
+	createdAt time.Time
+}
+
 func main() {
-	firstName := getUserData("Please enter your first name: ")
-	lastName := getUserData("Please enter your last name: ")
-	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+	userFirstName := getUserData("Please enter your first name: ")
+	userLastName := getUserData("Please enter your last name: ")
+	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
 
-	// ... do something awesome with that gathered data!
+	var userData user
 
-	fmt.Println(firstName, lastName, birthdate)
+	userData = user{
+		firstName: userFirstName,
+		lastName:  userLastName,
+		birthDate: userBirthdate,
+		createdAt: time.Now(),
+	}
+
+	outputUserDetails(userData)
+
 }
 
 func getUserData(promptText string) string {
@@ -19,4 +35,10 @@ func getUserData(promptText string) string {
 	var value string
 	fmt.Scan(&value)
 	return value
+}
+
+func outputUserDetails(u user) {
+
+	fmt.Println(u.firstName, u.lastName, u.birthDate)
+
 }
