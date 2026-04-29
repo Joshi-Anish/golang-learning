573b44e82777d9a5b6969c9c5899589c19db66c1
Joshi-Anish <anish.221450@ncit.edu.np>
2026-04-29 12:25:28 +0545

    pointers learned

---
+++ basic/Pointer-basic/go.mod
@@ -0,0 +1,3 @@
+module example.com/pointer-concept
+
+go 1.25.0
--- basic/Pointer-basic/pointer.go
+++ basic/Pointer-basic/pointer.go
@@ -5,6 +5,18 @@ import (
 )
 
 func main() {
-	fmt.Print("hello")
+	age := 32 //regular variable
 
+	userAge := &age //pointer which store age address
+
+	fmt.Println("Age address", userAge)
+
+	adultYear := getAdultYears(userAge)
+	fmt.Println(adultYear)
+
+}
+
+// we have called pointer and used it . so pointer cha vanae use * to get value of that address
+func getAdultYears(userAge *int) int {
+	return *userAge - 18
 }
