dae1799710566d64febac6f7984bc4ee59ae5131
Joshi-Anish <joshianish222@gmail.com>
2026-05-01 22:03:06 +0545

    todo updated

--- Practice/TODO/main.go
+++ Practice/TODO/main.go
@@ -1,51 +1,63 @@
 package main
 
 import (
-	// "errors"
 	"bufio"
-	"example.com/Note/note"
+	"example.com/todo/note"
+	"example.com/todo/todo"
 	"fmt"
 	"os"
 	"strings"
 )
 
 func main() {
+
 	title, content := getNoteData()
 
 	userNote, err := note.New(title, content)
-
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
+	todoText := getUserInput("Enter todo: ")
+
+	todoItem, err := todo.New(todoText)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	todoItem.Display()
+
+	err = todoItem.Save()
+	if err != nil {
+		fmt.Println("error while saving todo file")
+		return
+	}
+	fmt.Println("todo file saved")
+
 	userNote.Display()
+
 	err = userNote.Save()
 	if err != nil {
 		fmt.Println("error while saving file")
 		return
 	}
 	fmt.Println("file saved")
-
 }
 
 func getNoteData() (string, string) {
-	title := getUserInput("Enter title:")
-	content := getUserInput("Enter the content:")
-
+	title := getUserInput("Enter title: ")
+	content := getUserInput("Enter the content: ")
 	return title, content
 }
+
 func getUserInput(prompt string) string {
 	fmt.Print(prompt)
 
 	reader := bufio.NewReader(os.Stdin)
-	text, err := reader.ReadString('\n')
+	text, _ := reader.ReadString('\n')
 
-	if err != nil {
-		return ""
-	}
-	text = strings.TrimSuffix(text, "\n")
-	text = strings.TrimSuffix(text, "\r")
-
+	text = strings.TrimSpace(text)
 	return text
 }
