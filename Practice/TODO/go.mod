d68d10deb4ae23b60469e70afce006cdca55adee
Joshi-Anish <joshianish222@gmail.com>
2026-05-01 21:22:58 +0545

    new project started todo

---
+++ Practice/TODO/go.mod
@@ -0,0 +1,3 @@
+module example.com/todo
+
+go 1.25.0
---
+++ Practice/TODO/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	// "errors"
+	"bufio"
+	"example.com/Note/note"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	title, content := getNoteData()
+
+	userNote, err := note.New(title, content)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	userNote.Display()
+	err = userNote.Save()
+	if err != nil {
+		fmt.Println("error while saving file")
+		return
+	}
+	fmt.Println("file saved")
+
+}
+
+func getNoteData() (string, string) {
+	title := getUserInput("Enter title:")
+	content := getUserInput("Enter the content:")
+
+	return title, content
+}
+func getUserInput(prompt string) string {
+	fmt.Print(prompt)
+
+	reader := bufio.NewReader(os.Stdin)
+	text, err := reader.ReadString('\n')
+
+	if err != nil {
+		return ""
+	}
+	text = strings.TrimSuffix(text, "\n")
+	text = strings.TrimSuffix(text, "\r")
+
+	return text
+}
