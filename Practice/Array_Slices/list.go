af05bf8ffd69ce4deb1c1966144c22077be2cf5a
Joshi-Anish <joshianish222@gmail.com>
2026-05-12 12:16:53 +0545

    dyanmic way of using array learned

--- Practice/Array_Slices/list.go
+++ Practice/Array_Slices/list.go
@@ -4,22 +4,33 @@ import (
 	"fmt"
 )
 
+// dynamic array
 func main() {
-
-	//like here we used [o]value and doesnt used 1 2 3 value of array
-	productName := [4]string{"anish"}
-	prices := [3]float64{2.2, 3.0, 12.2}
+	prices := []float64{2.2, 1, 0}
 	fmt.Println(prices)
 
-	//we used the 3rd vakue of array
-	productName[2] = "joshi"
-	fmt.Println(productName)
+	//updating this slice dynamically
 
-	//slices
-	newPrices := prices[1:3]
-	fmt.Println(newPrices)
-
-	//slices inside slices
-	hightedPrice := newPrices[:1]
-	fmt.Println(hightedPrice)
+	prices = append(prices, 3.3)
+	fmt.Println(prices)
 }
+
+// func main() {
+
+// 	//like here we used [o]value and doesnt used 1 2 3 value of array
+// 	productName := [4]string{"anish"}
+// 	prices := [3]float64{2.2, 3.0, 12.2}
+// 	fmt.Println(prices)
+
+// 	//we used the 3rd vakue of array
+// 	productName[2] = "joshi"
+// 	fmt.Println(productName)
+
+// 	//slices
+// 	newPrices := prices[1:3]
+// 	fmt.Println(newPrices)
+
+// 	//slices inside slices
+// 	hightedPrice := newPrices[:1]
+// 	fmt.Println(hightedPrice)
+// }
