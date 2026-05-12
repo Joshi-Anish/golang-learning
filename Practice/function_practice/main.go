57bc80e66677e53ce876c7997b93db4bb3eac711
Joshi-Anish <joshianish222@gmail.com>
2026-05-12 15:42:10 +0545

    function double

--- Practice/function_practice/main.go
+++ Practice/function_practice/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 
+	numbers := []int{2, 3, 4}
+	double := doubleNumber(&numbers)
+	fmt.Println(double)
+
+}
+func doubleNumber(numbers *[]int) []int {
+
+	dNumber := []int{}
+
+	for _, value := range *numbers {
+		dNumber = append(dNumber, value*2)
+	}
+	return dNumber
 }
