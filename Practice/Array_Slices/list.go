27a90b9a8372ba51adb1b81f9f7117ed1614295b
Joshi-Anish <joshianish222@gmail.com>
2026-05-12 11:15:30 +0545

    use of array in different way learned

--- Practice/Array_Slices/list.go
+++ Practice/Array_Slices/list.go
@@ -5,6 +5,13 @@ import (
 )
 
 func main() {
+
+	//like here we used [o]value and doesnt used 1 2 3 value of array
+	productName := [4]string{"anish"}
 	prices := [3]float64{2.2, 3.0, 12.2}
 	fmt.Println(prices)
+
+	//we used the 3rd vakue of array
+	productName[2] = "joshi"
+	fmt.Println(productName)
 }
