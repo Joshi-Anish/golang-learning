45b8897d21c268a7dd918d87100a16b582639574
Joshi-Anish <joshianish222@gmail.com>
2026-05-12 14:37:40 +0545

    array slices map and for loop for them learned and completed

--- Practice/Array_Slices/main.go
+++ Practice/Array_Slices/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	username := make([]string, 2, 5)
+	//2 means front ma 2 empty slot hunxa and 5 vanae ko memeory capactiy allocate gardiya ko
+	username[0] = "anish"
+	username = append(username, "joshi")
+
+	fmt.Println(username)
+
+	for index, value := range username {
+		fmt.Println(index)
+		fmt.Println(value)
+	}
 }
