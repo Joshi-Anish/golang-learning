116cf8a74edb8bf0a8cfd7527d19bb684f739eb5
Joshi-Anish <joshianish222@gmail.com>
2026-05-14 10:41:43 +0545

    basic done

--- Practice/Price_Calculator/main.go
+++ Practice/Price_Calculator/main.go
@@ -1,5 +1,22 @@
 package main
 
+import "fmt"
+
 func main() {
+	prices := []float64{10, 20, 30}
+	taxRate := []float64{0, 0.07, 0.1, 0.15}
+
+	result := make(map[float64][]float64)
+
+	for _, taxRate := range taxRate {
+		taxIncludedPrice := make([]float64, len(prices))
+
+		for priceIndex, prices := range prices {
+			taxIncludedPrice[priceIndex] = prices * (1 + taxRate)
+		}
+		result[taxRate] = taxIncludedPrice
+
+	}
+	fmt.Println(result)
 
 }
